91f0cf63156174012e6f5c898adf627a26ecead8
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-12-31 03:53:41 +0530

    Added comment

--- backend/src/routes/travelAgent.routes.ts
+++ backend/src/routes/travelAgent.routes.ts
@@ -7,7 +7,7 @@ const travelAgentRouter = express.Router();
 travelAgentRouter.post("/planMytrip", async (req, res, next) => {
     await planMyTrip(req, res);
 });
-
+//This endpoint is for testing purposes.
 travelAgentRouter.post("/fetchFlightData", async (req, res, next) => {
     await fetchLocalFlightData(req, res);
 });
